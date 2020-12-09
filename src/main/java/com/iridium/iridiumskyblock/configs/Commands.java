60af6755c86347510b51c34cfabc7f96675d906a
Faun471 <bearbrand949@gmail.com>
2020-12-09 11:49:16 +0800

    bruh

--- src/main/java/com/iridium/iridiumskyblock/configs/Commands.java
+++ src/main/java/com/iridium/iridiumskyblock/configs/Commands.java
@@ -52,9 +52,6 @@ public class Commands {
     public AddValueCommand addValueCommand = new AddValueCommand();
     public RemoveValueCommand removeValueCommand = new RemoveValueCommand();
     public ExpelCommand expelCommand = new ExpelCommand();
-<<<<<<< Updated upstream
-=======
     public PurgeCommand purgeCommand = new PurgeCommand();
     public ResetCrystalsCommand resetCrystalsCommand = new ResetCrystalsCommand();
->>>>>>> Stashed changes
 }
